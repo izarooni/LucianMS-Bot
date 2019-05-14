fca31839d51aa52636e512f3f38e50a2f4d32d20
Ian <coalasonly@gmail.com>
2019-05-14 05:04:42 -0600

    Created 'news' bot command

--- src/main/java/com/lucianms/commands/worker/cmds/News.java
+++ src/main/java/com/lucianms/commands/worker/cmds/News.java
@@ -1,8 +1,12 @@
 package com.lucianms.commands.worker.cmds;
 
+import com.lucianms.Discord;
 import com.lucianms.commands.Command;
 import com.lucianms.commands.worker.BaseCommand;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+import sx.blah.discord.util.EmbedBuilder;
+
+import java.sql.*;
 
 /**
  * @author izarooni
@@ -20,5 +24,38 @@ public class News extends BaseCommand {
 
     @Override
     public void invoke(MessageReceivedEvent event, Command command) {
+        String message = command.concatFrom(0, " ");
+        String[] split = message.split(" \\| ");
+        if (split.length != 2) {
+            EmbedBuilder embed = createEmbed()
+                    .withTitle("How to use the command")
+                    .appendField("description", getDescription(), false)
+                    .appendDesc("\r\n**syntax**: ").appendDesc(getName()).appendDesc(" <news/updates> <content>");
+            createResponse(event).withEmbed(embed.build()).build();
+            return;
+        }
+        String title = split[0];
+        String content = split[1];
+        try (Connection con = Discord.getMapleConnection()) {
+            try (PreparedStatement ps = con.prepareStatement("insert into _web_posts values (default, ?, 'news', ?, ?)", Statement.RETURN_GENERATED_KEYS)) {
+                ps.setString(1, title);
+                ps.setString(2, content);
+                ps.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
+                ps.executeUpdate();
+                try (ResultSet rs = ps.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        EmbedBuilder embed = createEmbed()
+                                .appendDesc("News post successfully posted.")
+                                .appendDesc("\r\nhttp://maplechirithy.com/bulletin/" + rs.getInt(1));
+                        createResponse(event).withEmbed(embed.build()).build();
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            EmbedBuilder embed = createEmbed()
+                    .withTitle("Failed to create a news post")
+                    .withDescription("```\r\n" + e.toString() + "\r\n```");
+            createResponse(event).withEmbed(embed.build()).build();
+        }
     }
 }
