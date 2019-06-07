0cee1b88e9be0a3f9ef423bf0ff29c04f3e3028c
Ian <coalasonly@gmail.com>
2019-06-07 10:43:53 -0600

    Bot issue with sending a message to a non-existing channel

--- src/main/java/com/lucianms/net/maple/handlers/MessageChannel.java
+++ src/main/java/com/lucianms/net/maple/handlers/MessageChannel.java
@@ -2,15 +2,20 @@ package com.lucianms.net.maple.handlers;
 
 import com.lucianms.Discord;
 import com.lucianms.utils.packet.receive.MaplePacketReader;
+import sx.blah.discord.handle.obj.IChannel;
 
 /**
  * @author izarooni
  */
 public class MessageChannel extends DiscordResponse {
+
     @Override
     public void handle(MaplePacketReader reader) {
         long channelID = reader.readLong();
         String content = reader.readMapleAsciiString();
-        Discord.getBot().getClient().getChannelByID(channelID).sendMessage(content);
+        IChannel channel = Discord.getBot().getClient().getChannelByID(channelID);
+        if (channel != null) {
+            channel.sendMessage(content);
+        }
     }
 }
